39114659f9890c191f054a805c300e2bb4dec433
nikitaOsipof <nikita@ifmo.spb.ru>
2025-01-14 22:59:19 +0300

    Пакеты

---
+++ mytest.py
@@ -0,0 +1,18 @@
+import pysimlib.mymath as myoper
+
+sm = myoper.myadd(3,69)
+print(sm)
+
+sm = myoper.mymul(3,69)
+print(sm)
+
+su = myoper.myqrt3(69)
+print(su)
+
+su = myoper.mydiv(6,9)
+print(su)
+
+from pysimlib import mymath as oper
+
+sm = oper.mymul(3,69)
+print(sm)
