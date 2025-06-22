f36146fd86b806bc55a2ae8efbeaf548efa5ac16
nikitaOsipof <nikita@ifmo.spb.ru>
2025-06-22 23:40:22 +0300

    Пакеты

--- mod_0/pythoncode_00.py
+++ mod_0/pythoncode_00.py
@@ -3,6 +3,6 @@ from datetime import datetime
 right_this_minute = datetime.today().minute
 
 if right_this_minute % 2 == 0:
-    print("This minute")
+    print("This minute", str(datetime.today()))
 else:
-    print("Not minute.")
+    print("Not minute.", repr(datetime.today()))
