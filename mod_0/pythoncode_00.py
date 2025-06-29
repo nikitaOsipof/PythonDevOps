7df962cb04c82534843236eb8241a7c718414e75
nikitaOsipof <nikita@ifmo.spb.ru>
2025-06-29 16:01:45 +0300

    Работа с данными

--- mod_0/pythoncode_00.py
+++ mod_0/pythoncode_00.py
@@ -2,7 +2,15 @@ from datetime import datetime
 
 right_this_minute = datetime.today().minute
 
+print("str", str(datetime.today()))
+print("repr", repr(datetime.today()))
+
+'''
+
 if right_this_minute % 2 == 0:
     print("This minute", str(datetime.today()))
 else:
     print("Not minute.", repr(datetime.today()))
+
+'''
+
